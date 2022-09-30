5c701f9dc9bb94cdd9cb3d4c3936cffe30278330
Cainan Schiavolin <65777339+ItzCrynix@users.noreply.github.com>
2022-09-30 20:07:18 -0300

    Nova tabela e Pasta para imagens
    Criada uma nova tabela com as médias dos valores, separados por ano.

--- Main.r
+++ Main.r
@@ -5,7 +5,17 @@ cepagri <- read.csv(arquivo, header = FALSE, sep = ";", col.names = nomeColunas)
 cepagri <- na.omit(cepagri) # Remove os NA's do dataframe
 summary(cepagri)
 
-cepagri$temp <- as.double(cepagri$temp)
-horarios[1]
+cepagri$temp <- as.numeric(cepagri$temp)
+cepagri$horario <- strptime(cepagri$horario, "%d/%m/%Y-%H:%M")
 
-plot(cepagri)
+mediasPorAno <- c()
+for (ano in unique(cepagri$horario$year)) {
+  mediaTemp <- mean(cepagri$temp[cepagri$horario$year == ano])
+  mediaVento <- mean(cepagri$vento[cepagri$horario$year == ano])
+  mediaUmid <- mean(cepagri$umid[cepagri$horario$year == ano])
+  mediaSensa <- mean(cepagri$sensa[cepagri$horario$year == ano])
+  mediasPorAno <- rbind(mediasPorAno, c(mediaTemp, mediaVento, mediaUmid, mediaSensa))
+}
+
+dimnames(mediasPorAno) <- list(unique(cepagri$horario$year) + 1900, c("Temp", "Vento", "Umidade", "Sensacao"))
+
