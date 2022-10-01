8e481316824f1f5d2c8b70a6b764dd754d3eaea3
Cainan Schiavolin <65777339+ItzCrynix@users.noreply.github.com>
2022-09-30 21:14:04 -0300

    Update Main.r

--- Main.r
+++ Main.r
@@ -8,14 +8,16 @@ summary(cepagri)
 cepagri$temp <- as.numeric(cepagri$temp)
 cepagri$horario <- strptime(cepagri$horario, "%d/%m/%Y-%H:%M")
 
-mediasPorAno <- c()
+mediasPorAno <- data.frame()
 for (ano in unique(cepagri$horario$year)) {
   mediaTemp <- mean(cepagri$temp[cepagri$horario$year == ano])
   mediaVento <- mean(cepagri$vento[cepagri$horario$year == ano])
   mediaUmid <- mean(cepagri$umid[cepagri$horario$year == ano])
   mediaSensa <- mean(cepagri$sensa[cepagri$horario$year == ano])
-  mediasPorAno <- rbind(mediasPorAno, c(mediaTemp, mediaVento, mediaUmid, mediaSensa))
+  mediasPorAno <-rbind(mediasPorAno, data.frame(mediaTemp, mediaVento, mediaUmid, mediaSensa, row.names = ano + 1900))
 }
 
-dimnames(mediasPorAno) <- list(unique(cepagri$horario$year) + 1900, c("Temp", "Vento", "Umidade", "Sensacao"))
-
+grafTemp <- ggplot(mediasPorAno, aes(x = rownames(mediasPorAno), y = mediasPorAno$mediaTemp, group = 1))
+grafTemp <- grafTemp + geom_point()
+grafTemp <- grafTemp + geom_line()
+grafTemp <- grafTemp + xlab("Anos") + ylab("Temperatura") + ggtitle("Temperatura média/Ano")
