d8cc7866734631c2fe436e5c7f8dd8a4f864bed2
Cainan Schiavolin <65777339+ItzCrynix@users.noreply.github.com>
2022-10-01 20:12:40 -0300

    Update Main.r

--- Main.r
+++ Main.r
@@ -37,18 +37,50 @@ rownames(mediasPorAno) <- unique(cepagri$horario$year) + 1900; colnames(mediasPo
 
 # ------------------------------------ Tabela médias por mês ------------------------------------------------ #
 mediasPorMes <- data.frame()
+nomeLinhas <- c()
 for (ano in unique(cepagri$horario$year)) {
   for (mes in unique(cepagri[cepagri$horario$year == ano,]$horario$mon)) {
     mediasPorMes <- rbind(mediasPorMes, apply(cepagri[cepagri$horario$year == ano & cepagri[cepagri$horario$year == ano,]$horario$mon == mes, 2:5], 2, mean))
+    nomeLinhas <- c(nomeLinhas, paste(mes+1, "/", ano + 1900, sep = ""))
   }
 }
-colnames(mediasPorMes) <- c("Temp", "Vento", "Umidade", "Sensacao")
+colnames(mediasPorMes) <- c("Temp", "Vento", "Umidade", "Sensacao"); rownames(mediasPorMes) <- nomeLinhas
 # ----------------------------------------------------------------------------------------------------------- #
 
+# ------------------------------------------- tabela temperatura-sensação por mes ------------------------------------------- #
+tempSensaMes <- data.frame()
+for (ano in unique(cepagri$horario$year)) {
+  for (mes in unique(cepagri$horario$mon[cepagri$horario$year == ano])) {
+    mediaTemperatura <- mean(cepagri$temp[cepagri$horario$mon == mes & cepagri$horario$year == ano])
+    mediaSensacao <- mean(cepagri$sensa[cepagri$horario$mon == mes & cepagri$horario$year == ano])
+    tempSensaMes <- rbind(tempSensaMes,data.frame(mediaTemperatura,mediaSensacao,row.names = paste(mes+1,"/",ano + 1900,sep="")))
+  }
+}
+# -------------------------------------------- tabela ano 2014 ------------------------------------------------ #
+mediasAnoSeca <- data.frame()
+nomeLinhas <- c()
+for (mes in unique(cepagri$horario$mon[cepagri$horario$year == 114])) {
+  mediasAnoSeca <- rbind(mediasAnoSeca, quantile(cepagri[cepagri$horario$year == 114 & cepagri$horario$mon == mes,]$umid))
+  nomeLinhas <- c(nomeLinhas, paste(mes+1, "/", 2014, sep = ""))
+}
+rownames(mediasAnoSeca) <- nomeLinhas; colnames(mediasAnoSeca) <- c("Minimo", "1 quartil", "Mediana", "3 quartil", "Máximo")
+
 # --------------------------- Gráfico média temperatura durante os anos ----------------------------------- #
-grafTemp <- ggplot(mediasPorMes, aes(x = 1:nrow(mediasPorMes), group = 1,  weight = Umidade))
+grafTemp <- ggplot(mediasPorMes, aes(x = rownames(mediasPorMes), group = 1,  weight = Umidade))
 grafTemp <- grafTemp + geom_point(color=rgb(255, 0, 0, maxColorValue = 255), aes(y = Temp, group = 1))
 grafTemp <- grafTemp + geom_line(color=rgb(255, 0, 0, maxColorValue = 255), aes(y = Temp, group = 1))
 grafTemp <- grafTemp + theme_minimal()
 grafTemp <- grafTemp + xlab("Meses") + ylab("Temperatura") + ggtitle("Temperatura média por mês")
-# --------------------------------------------------------------------------------------------------------- #
+
+grafUmidadeAnoSeca <- ggplot(mediasAnoSeca, aes(x = rownames(mediasAnoSeca), group=1))
+grafUmidadeAnoSeca <- grafUmidadeAnoSeca + geom_point(color=rgb(0, 0, 255, maxColorValue = 255), aes(y = mediasAnoSeca$Minimo))
+grafUmidadeAnoSeca <- grafUmidadeAnoSeca + geom_point(color=rgb(0, 0, 0, maxColorValue = 255), aes(y = mediasAnoSeca$`1 quartil`))
+grafUmidadeAnoSeca <- grafUmidadeAnoSeca + geom_point(color=rgb(0, 255, 0, maxColorValue = 255), aes(y = mediasAnoSeca$Mediana))
+grafUmidadeAnoSeca <- grafUmidadeAnoSeca + geom_point(color=rgb(0, 0, 0, maxColorValue = 255), aes(y = mediasAnoSeca$`3 quartil`))
+grafUmidadeAnoSeca <- grafUmidadeAnoSeca + geom_point(color=rgb(255, 0, 0, maxColorValue = 255), aes(y = mediasAnoSeca$Máximo))
+grafUmidadeAnoSeca <- grafUmidadeAnoSeca + geom_line(color=rgb(0, 0, 255, maxColorValue = 255), aes(y = mediasAnoSeca$Minimo))
+grafUmidadeAnoSeca <- grafUmidadeAnoSeca + geom_line(color=rgb(0, 0, 0, maxColorValue = 255), aes(y = mediasAnoSeca$`1 quartil`))
+grafUmidadeAnoSeca <- grafUmidadeAnoSeca + geom_line(color=rgb(0, 255, 0, maxColorValue = 255), aes(y = mediasAnoSeca$Mediana))
+grafUmidadeAnoSeca <- grafUmidadeAnoSeca + geom_line(color=rgb(0, 0, 0, maxColorValue = 255), aes(y = mediasAnoSeca$`3 quartil`))
+grafUmidadeAnoSeca <- grafUmidadeAnoSeca + geom_line(color=rgb(255, 0, 0, maxColorValue = 255), aes(y = mediasAnoSeca$Máximo))
+grafUmidadeAnoSeca
