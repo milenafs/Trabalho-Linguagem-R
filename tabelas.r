eb605b290870916682c2446952195e49d29a8f1c
Gabriel Oliveira <74967198+gabrielosilva@users.noreply.github.com>
2022-10-02 22:46:14 -0300

    Update tabelas.r

--- tabelas.r
+++ tabelas.r
@@ -38,3 +38,21 @@ for (ano in unique(cepagri$horario$year)) {
     tempUmidadeMes <- rbind(tempUmidadeMes,data.frame(mediaTemperatura,mediaUmidade,row.names = paste(mes+1,"/",ano + 1900,sep=""))) # Junta os dois valores em um dataframe
   }
 }
+
+# ------------------------------------------- Tabela de condições extremas de 2020 ------------------------------------------- # 
+
+tabCondExtre <- data.frame(Temp=cepagri$temp[cepagri$temp > 35 & cepagri$horario$year == 120],Vento=cepagri$vento[cepagri$temp > 35 & cepagri$horario$year == 120],Umi=cepagri$umid[cepagri$temp > 35 & cepagri$horario$year == 120],Data=cepagri$horario[cepagri$temp > 35 & cepagri$horario$year == 120])
+tabCondExtre <- rbind(data.frame(Temp=cepagri$temp[cepagri$temp < 5 & cepagri$horario$year == 120],Vento=cepagri$vento[cepagri$temp < 5 & cepagri$horario$year == 120],Umi=cepagri$umid[cepagri$temp < 5 & cepagri$horario$year == 120],Data=cepagri$horario[cepagri$temp < 5 & cepagri$horario$year == 120]))
+tabCondExtre <- rbind(tabCondExtre ,data.frame(Temp=cepagri$temp[cepagri$vento>62 & cepagri$horario$year == 120],Vento=cepagri$vento[cepagri$vento>62 & cepagri$horario$year == 120],Umi=cepagri$umid[cepagri$vento>62 & cepagri$horario$year == 120],Data=cepagri$horario[cepagri$vento>62 & cepagri$horario$year == 120]))
+tabCondExtre <- rbind(tabCondExtre ,data.frame(Temp=cepagri$temp[cepagri$umid<20 & cepagri$horario$year == 120],Vento=cepagri$vento[cepagri$umid<20 & cepagri$horario$year == 120],Umi=cepagri$umid[cepagri$umid<20 & cepagri$horario$year == 120],Data=cepagri$horario[cepagri$umid<20 & cepagri$horario$year == 120]))
+
+# ------------------------------------------- Tabela com as temperaturas durante o inverno ------------------------------------------- # 
+
+tabInverno <- data.frame()
+for (ano in unique(cepagri$horario$year)) {
+  mediaTemperatura <- (mean(cepagri$temp[cepagri$horario$mon == 6 & cepagri$horario$year == ano])+
+                         mean(cepagri$temp[cepagri$horario$mon == 7 & cepagri$horario$year == ano])+
+                         mean(cepagri$temp[cepagri$horario$mon == 8 & cepagri$horario$year == ano]))/3
+  tabInverno <- rbind(tabInverno,data.frame(mediaTemperatura,ano=ano+1900))
+}
+tabInverno <- na.omit(tabInverno)
