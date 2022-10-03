9c3d1180ec03de0179d924a776e4b89df18b555b
Cainan Schiavolin <65777339+ItzCrynix@users.noreply.github.com>
2022-10-02 22:28:25 -0300

    Update graficos.r

--- graficos.r
+++ graficos.r
@@ -7,12 +7,15 @@ grafCondExtre <- grafCondExtre + geom_line(aes(y = Temp,colour="Temperatura"))
 grafCondExtre <- grafCondExtre + geom_line(aes(y = Vento,colour="Vento"))
 grafCondExtre <- grafCondExtre + geom_line(aes(y = Umi,colour="Umidade"))+theme_minimal();
 grafCondExtre <- grafCondExtre + xlab("Meses") + ylab("Valores(ºC % km/h)") + labs(colour="Legenda",title="Análise de Temperatura, Vento e Umidade de 2020")
-grafCondExtre# --------------------------- Gráfico média temperatura durante os anos ----------------------------------- #
+grafCondExtre
+
+# --------------------------- Gráfico média temperatura durante os anos ----------------------------------- #
 grafTemp <- ggplot(mediasPorMes, aes(x = 1:nrow(mediasPorMes), group = 1,  weight = Umidade))
 grafTemp <- grafTemp + geom_point(color=rgb(255, 0, 0, maxColorValue = 255), aes(y = Temp, group = 1))
 grafTemp <- grafTemp + geom_line(color=rgb(255, 0, 0, maxColorValue = 255), aes(y = Temp, group = 1))
 grafTemp <- grafTemp + theme_minimal()
 grafTemp <- grafTemp + xlab("Meses") + ylab("Temperatura") + ggtitle("Temperatura média por mês")
+# --------------------------------------------------------------------------------------------------------- #
 
 #----------------------------------Gráfico - correlação temperatura-sensação térmica mês--------------------------------------#
 
@@ -23,7 +26,7 @@ grafTempSensa <- grafTempSensa + geom_point(aes(y = mediaSensacao, colour = medi
 grafTempSensa <- grafTempSensa + scale_color_continuous(name =  "Média Temperatura", low = "blue", high = "red")
 grafTempSensa <- grafTempSensa + xlab("Meses") + ylab("Temperatura") + ggtitle("Correlação Temperatura e Sensação Térmica")
 grafTempSensa
-
+# --------------------------------------------------------------------------------------------------------- #
 
 #------------------------------gráfico preenchimento inverno-----------------------------------
 # 20 ou 21 de junho e acaba em 22 ou 23 de setembro.  inverno = julho agosto setembro
@@ -35,9 +38,10 @@ for (ano in unique(cepagri$horario$year)) {
   tabInverno <- rbind(tabInverno,data.frame(mediaTemperatura,ano=ano+1900))
 }
 tabInverno <- na.omit(tabInverno)
-grafInverno <- ggplot(tabInverno, aes(x = ano)) + geom_density(aes(y = ..count..),color="darkblue", fill="darkblue",alpha=0.25)
+grafInverno <- ggplot(tabInverno, aes(x = ano)) + geom_density(aes(y = ..count..),color="darkblue", fill="darkblue",alpha=0.25) # crfia um gráfico de densidade
 grafInverno <- grafInverno + xlab("Anos") + ylab("Nível da Média Témica") + ggtitle("Comparação da Média dos Invernos desde 2015 até 2021")
 grafInverno
+# --------------------------------------------------------------------------------------------------------- #
 
 #------------------------------gráfico de condições climáticas extremas--------------------------------#
 #condições extremas: temp > 35 < 5, vento > 62 umidade < 10%
